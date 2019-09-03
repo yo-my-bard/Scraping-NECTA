6314c8157a86e80b77a2b8e3698ed28b7fd5c924
Yusuph <yusuph.mka@outlook.com>
2019-09-02 17:42:13 -0700

    Updated items fieldset for ACSEE to include metadata tables

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -19,5 +19,12 @@ class PsleItem(scrapy.Item):
 
 class AcseeItem(scrapy.Item):
     url = scrapy.Field()
+    status = scrapy.Field()
+    exam_center = scrapy.Field()
     is_error = scrapy.Field()
-    tables = scrapy.Field()
+    errors = scrapy.Field()
+    html = scrapy.Field()
+    result_table = scrapy.Field() #header = 0
+    rankings_table = scrapy.Field() #category, rank
+    div_performance_table = scrapy.Field() #header = 0
+    subject_performance_table = scrapy.Field() #header = 0
